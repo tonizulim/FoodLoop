8024600de407c4109d5e6e9f1c2e8263960c0f92
tonizulim <toni.zulim003@gmail.com>
2025-12-18 18:43:27 +0100

    temp fix3

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -1,9 +1,9 @@
+"use Client";
 import ItemPageContent from "@/components/ItemPageContent";
-import { ItemPageProps } from "@/types/ItemPageProps";
 
 // export default async function ItemPage({ params }: ItemPageProps) {
 //   const itemId = Number(params.id);
 
-export default async function ItemPage({ params }: ItemPageProps) {
+export default function ItemPage() {
   return <ItemPageContent id={1} />;
 }
