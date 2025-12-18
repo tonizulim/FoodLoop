7a7fd8abee8970ae70ab72f2e30acbe86d692972
tonizulim <toni.zulim003@gmail.com>
2025-12-18 18:41:40 +0100

    temp fix2

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -1,8 +1,8 @@
 import ItemPageContent from "@/components/ItemPageContent";
 import { ItemPageProps } from "@/types/ItemPageProps";
 
-//export default async function ItemPage({ params }: ItemPageProps) {
-//const itemId = Number(params.id);
+// export default async function ItemPage({ params }: ItemPageProps) {
+//   const itemId = Number(params.id);
 
 export default async function ItemPage({ params }: ItemPageProps) {
   return <ItemPageContent id={1} />;
