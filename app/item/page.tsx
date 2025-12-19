6bb64ded5f8aec6a91cfae53f93268aa67211edd
tonizulim <toni.zulim003@gmail.com>
2025-12-19 09:47:04 +0100

    added item page

---
+++ app/item/page.tsx
@@ -0,0 +1,9 @@
+"use Client";
+import ItemPageContent from "@/components/ItemPageContent";
+
+// export default async function ItemPage({ params }: ItemPageProps) {
+//   const itemId = Number(params.id);
+
+export default function ItemPage() {
+  return <ItemPageContent id={1} />;
+}
