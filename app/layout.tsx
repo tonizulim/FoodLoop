05c2eaac02b48cdbbf97b51083ea3123b710fb8b
tonizulim <toni.zulim003@gmail.com>
2026-02-25 21:52:09 +0100

    my fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,29 +21,16 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "FoodLoop",
   description: "A community platform for sharing food and reducing waste",
+  manifest: "/site.webmanifest",
   icons: {
-    // icon: [
-    //   { url: "/logo.svg", type: "image/svg+xml" },
-    //   { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    //   { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    //   { url: "/favicon.ico" },
-    // ],
-    // shortcut: ["/favicon.ico"],
-    // apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
-
-    //ovo radi na firefox
     icon: [
-      { url: "/favicon.ico" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    shortcut: [{ url: "/favicon.ico" }],
-    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
-
-    //ovo radi na chrome
-    // icon: "/logo.svg",
-    // shortcut: "/logo.svg",
-    // apple: "/apple-touch-icon.png", // MUST be PNG for iOS
+    shortcut: ["/favicon-32x32.png"],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
   },
 };
 
