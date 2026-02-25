0e81fadcae1732d0d4ef2185d84d03fdf0926077
tonizulim <toni.zulim003@gmail.com>
2026-02-25 21:45:24 +0100

    my fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,25 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/logo.svg", type: "image/svg+xml" },
-      { url: "/favicon.ico" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.ico" },
     ],
-    shortcut: [{ url: "/favicon.ico" }],
+    shortcut: ["/favicon.ico"],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    //ovo radi na firefox
+    // icon: [
+    //   { url: "/favicon.ico" },
+    //   { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    //   { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    // ],
+    // shortcut: [{ url: "/favicon.ico" }],
+    // apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+
+    //ovo radi na chrome
+    // icon: "/logo.svg",
+    // shortcut: "/logo.svg",
+    // apple: "/apple-touch-icon.png", // MUST be PNG for iOS
   },
 };
 
